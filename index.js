8351466b1b415d2675453868d534de4aa1ea8c5e
Lauren Slayman <laurenaslayman@gmail.com>
2023-03-30 15:50:27 -0600

    pass tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(driverName, searchCriteria) {
+    return driverName.filter(
+        (searchQuery) => searchQuery.toLowerCase() === searchCriteria.toLowerCase()
+    );
+}
+
+function fuzzyMatch(driverName, providedLetter) {
+    return driverName.filter(
+        (searchQuery) => 
+            searchQuery.toLowerCase().indexOf(providedLetter.toLowerCase()) === 0
+    )
+}
+
+function matchName (driverName, searchName) {
+    return driverName.filter((query) => query.name === searchName)
+}
